9d7540612db3b32a4b95ccad487ecf19c8f64565
Erik Lund <culex.eriklund@gmail.com>
2011-08-29 20:32:17 +0200

    Made an awkward camera controller

--- src/voxicity/Voxicity.java
+++ src/voxicity/Voxicity.java
@@ -1,10 +1,12 @@
 package voxicity;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.GLU;
+import org.lwjgl.Sys;
 
 public class Voxicity
 {
@@ -14,7 +16,10 @@ public class Voxicity
 		System.setProperty( "java.library.path", "native" );
 	}
 
+	long last_frame = 0;
 	float rot = 0;
+
+	float camera[] = new float[3];
 
 	public void init()
 	{
@@ -29,87 +34,109 @@ public class Voxicity
 			System.exit(0);
 		}
 
+		get_time_delta();
+		setup_camera();
+
+		while ( !Display.isCloseRequested() )
+		{
+			update( get_time_delta() );
+			draw();
+		}
+			Display.destroy();
+	}
+
+	int get_time_delta()
+	{
+		// Get the time in milliseconds
+		long new_time = (Sys.getTime() * 1000)  / Sys.getTimerResolution();
+		int delta = (int) ( new_time - last_frame );
+		last_frame = new_time;
+		return delta;
+	}
+
+	void update( int delta )
+	{
+		if ( Keyboard.isKeyDown( Keyboard.KEY_A ) ) camera[0] -= 0.35 * delta;
+		if ( Keyboard.isKeyDown( Keyboard.KEY_D ) ) camera[0] += 0.35 * delta;
+
+		if ( Keyboard.isKeyDown( Keyboard.KEY_W ) ) camera[2] -= 0.35 * delta;
+		if ( Keyboard.isKeyDown( Keyboard.KEY_S ) ) camera[2] += 0.35 * delta;
+
+		if ( Keyboard.isKeyDown( Keyboard.KEY_SPACE ) ) camera[1] += 0.35 * delta;
+		if ( Keyboard.isKeyDown( Keyboard.KEY_C ) ) camera[1] -= 0.35 * delta;
+
+		rot += 0.15 * delta;
+	}
+
+	void draw()
+	{
+		GL11.glLoadIdentity();
+		GLU.gluLookAt( camera[0], camera[1], camera[2], 0,0, -80, 0,1,0 );
+		GL11.glTranslatef( 0, 0, -80 );
+		//GL11.glRotatef( rot, 0, 1, 1 );
+		GL11.glTranslatef( 0, 0, 80 );
+
+		// Clear the screen and depth buffer
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+		GL11.glBegin(GL11.GL_QUADS);
+
+		// Front
+		GL11.glColor3f( 1.0f, 0.0f, 0.0f );
+		GL11.glVertex3f( -10, -10, -90 );
+		GL11.glColor3f( 0.0f, 1.0f, 0.0f );
+		GL11.glVertex3f( 10, -10, -90 );
+		GL11.glColor3f(0.5f,0.5f,1.0f);
+		GL11.glVertex3f( 10, 10, -90 );
+		GL11.glColor3f( 0.0f, 0.0f, 1.0f );
+		GL11.glVertex3f( -10, 10, -90 );
+
+		// left
+		GL11.glVertex3f( -10, -10, -90 );
+		GL11.glVertex3f( -10, -10, -70 );
+		GL11.glVertex3f( -10, 10, -70 );
+		GL11.glVertex3f( -10, 10, -90 );
+
+		// right
+		GL11.glVertex3f( 10, -10, -90 );
+		GL11.glVertex3f( 10, -10, -70 );
+		GL11.glVertex3f( 10, 10, -70 );
+		GL11.glVertex3f( 10, 10, -90 );
+
+		// front
+		GL11.glColor3f( 0.5f, 1.0f, 0.0f );
+		GL11.glVertex3f( -10, -10, -70 );
+		GL11.glVertex3f( 10, -10, -70 );
+		GL11.glVertex3f( 10, 10, -70 );
+		GL11.glVertex3f( -10, 10, -70 );
+
+		// top
+		GL11.glColor3f( 1.0f, 0.5f, 0.0f );
+		GL11.glVertex3f( -10, 10, -90 );
+		GL11.glVertex3f( -10, 10, -70 );
+		GL11.glVertex3f( 10, 10, -70 );
+		GL11.glVertex3f( 10, 10, -90 );
+
+		// bottom
+		GL11.glColor3f( 0.0f, 0.5f, 1.0f );
+		GL11.glVertex3f( -10, -10, -90 );
+		GL11.glVertex3f( -10, -10, -70 );
+		GL11.glVertex3f( 10, -10, -70 );
+		GL11.glVertex3f( 10, -10, -90 );
+		GL11.glEnd();
+
+		Display.update();
+	}
+
+	void setup_camera()
+	{
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
-//		GL11.glOrtho(0, 800, 600, 0, 1, -1);
 		GLU.gluPerspective( 45.0f, 1.333f, 1f, 1000f );
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
 		GL11.glShadeModel( GL11.GL_SMOOTH );
 		GL11.glEnable( GL11.GL_DEPTH_TEST );
-
-		while ( !Display.isCloseRequested() )
-		{
-			rot += 0.5;
-
-			GL11.glLoadIdentity();
-			GLU.gluLookAt( 0, 50, 0, 0,0, -90, 0,1,0 );
-			GL11.glTranslatef( 0, 0, -90 );
-			GL11.glRotatef( rot, 0, 1, 1 );
-			GL11.glTranslatef( 0, 0, 90 );
-
-			// Clear the screen and depth buffer
-			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);	
-
-			// set the color of the quad (R,G,B,A)
-			GL11.glColor3f(0.5f,0.5f,1.0f);
-
-/*			// draw quad
-			GL11.glBegin(GL11.GL_QUADS);
-			   GL11.glVertex2f(100,100);
-			   GL11.glVertex2f(100+200,100);
-			   GL11.glVertex2f(100+200,100+200);
-			   GL11.glVertex2f(100,100+200);
-			GL11.glEnd();
-*/
-		GL11.glColor3f( 1.0f, 0.0f, 0.0f );
-		GL11.glBegin(GL11.GL_QUADS);
-		   // Front
-		   GL11.glVertex3f( -10, -10, -90 );
-		   GL11.glColor3f( 0.0f, 1.0f, 0.0f );
-		   GL11.glVertex3f( 10, -10, -90 );
-			GL11.glColor3f(0.5f,0.5f,1.0f);
-		   GL11.glVertex3f( 10, 10, -90 );
-		   GL11.glColor3f( 0.0f, 0.0f, 1.0f );
-		   GL11.glVertex3f( -10, 10, -90 );
-
-			// left
-		  GL11.glVertex3f( -10, -10, -90 );
-			GL11.glVertex3f( -10, -10, -70 );
-			GL11.glVertex3f( -10, 10, -70 );
-			GL11.glVertex3f( -10, 10, -90 );
-
-			// right
-			GL11.glVertex3f( 10, -10, -90 );
-			GL11.glVertex3f( 10, -10, -70 );
-			GL11.glVertex3f( 10, 10, -70 );
-			GL11.glVertex3f( 10, 10, -90 );
-
-			// front
-			GL11.glColor3f( 0.5f, 1.0f, 0.0f );
-			GL11.glVertex3f( -10, -10, -70 );
-			GL11.glVertex3f( 10, -10, -70 );
-			GL11.glVertex3f( 10, 10, -70 );
-			GL11.glVertex3f( -10, 10, -70 );
-
-			// top
-			GL11.glColor3f( 1.0f, 0.5f, 0.0f );
-			GL11.glVertex3f( -10, 10, -90 );
-			GL11.glVertex3f( -10, 10, -70 );
-			GL11.glVertex3f( 10, 10, -70 );
-			GL11.glVertex3f( 10, 10, -90 );
-
-			// bottom
-			GL11.glColor3f( 0.0f, 0.5f, 1.0f );
-			GL11.glVertex3f( -10, -10, -90 );
-			GL11.glVertex3f( -10, -10, -70 );
-			GL11.glVertex3f( 10, -10, -70 );
-			GL11.glVertex3f( 10, -10, -90 );
-		GL11.glEnd();
-			Display.update();
-		}
-
-		Display.destroy();
 	}
 
 	public static void main( String[] args )
