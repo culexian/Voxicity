b713550421ecb68fedfbc9a5f2a801844197c08c
Erik Lund <culex.eriklund@gmail.com>
2012-07-25 12:11:14 +0200

    Removed unused imports

--- src/voxicity/ChunkServer.java
+++ src/voxicity/ChunkServer.java
@@ -25,9 +25,6 @@ import java.util.LinkedList;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.PriorityBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 
 import org.lwjgl.util.vector.Vector3f;
 
