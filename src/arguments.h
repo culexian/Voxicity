f6a008b3358b9193dadbc21ed58201906cf349c0
LawlingBear <karl@transit.no>
2014-07-28 00:49:38 +0200

    Changed some fallacies

--- src/arguments.h
+++ src/arguments.h
@@ -8,16 +8,54 @@ class Arguments
 {
 private:
 	// Map for pairs of string( --option argument )
-	unordered_map< std::string, std::string > pairs;
+	std::unordered_map< std::string, std::string > pairs;
 
 	// Map for flags( -abc )
-	unordered_set< char > flags;
+	std::unordered_set< char > flags;
+
+	void parse_pairs( argc, argv[] )
+	{
+		for ( int i = 0 ; i < argc ; i++ )
+		{
+			// Check if the string matches two hyphens
+			// followed by any number of characters and
+			// then check that there is at least one more
+			// string in args.
+			if ( argv[i].matches( "--.*" ) && ( i + 1 <= argc ) )
+			{
+				if ( argv[i + 1].matches( "--.*" ) )
+					std::cout << "\"" << argv[i] << " " << args[i + 1] << "\" is not an option pair.\n";
+
+				// Put the new argument pair in the map,
+				// overwriting any previous pair with the
+				// same key.
+				pairs.insert( argv[i].substr( 2 ), argv[i + 1] );
+
+				// Skip one string ahead over the pair
+				i++;
+			}
+		}
+	}
+
+	void parse_flags ( argc, argv[] )
+	{
+		for ( int i = 0 ; i < argc ; i++ )
+		{
+			// Check that the string matches the flag option format
+			if ( argv[i].matches( "-\\p{Alnum}?" ) )
+			{
+				// Add each flag in the string to the flags set
+				for (int j = 1 ; j < argv[i].length() ; j++ )
+					flags.insert( argv[i].at( j ) );
+			}
+		}
+	}
 
 public:
 
 	// Takes an array of Strings as an argument and constructs
 	// the maps of pairs and flags from it
-	Arguments( int argc, char* argv[] )
+	Arguments( argc, argv[] )
 	{
 		parse_pairs( int argc, char* argv[] );
 		parse_flags( int argc, char* argv[] );
@@ -43,47 +81,5 @@ public:
 		return flags.contains( key );
 	}
 
-	// Return whether or not the flag is present
-	public bool get_flag( char key )
-	{
-		return get_flag( new char( key ) );
-	}
 
-	void parse_pairs( int argc, char* argv[] )
-	{
-		for ( int i = 0 ; i < argc ; i++ )
-		{
-			// Check if the string matches two hyphens
-			// followed by any number of characters and
-			// then check that there is at least one more
-			// string in args.
-			if ( argv[i].matches( "--.*" ) && ( i + 1 <= argc ) )
-			{
-				if ( argv[i + 1].matches( "--.*" ) )
-					std::cout << "\"" << argv[i] << " " << args[i + 1] << "\" is not an option pair.\n";
-
-				// Put the new argument pair in the map,
-				// overwriting any previous pair with the
-				// same key.
-				pairs.insert( argv[i].substr( 2 ), argv[i + 1] );
-
-				// Skip one string ahead over the pair
-				i++;
-			}
-		}
-	}
-
-	void parse_flags ( int argc, char* argv[] )
-	{
-		for ( int i = 0 ; i < argc ; i++ )
-		{
-			// Check that the string matches the flag option format
-			if ( argv[i].matches( "-\\p{Alnum}?" ) )
-			{
-				// Add each flag in the string to the flags set
-				for (int j = 1 ; j < argv[i].length() ; j++ )
-					flags.insert( argv[i].at( j ) );
-			}
-		}
-	}
 }
