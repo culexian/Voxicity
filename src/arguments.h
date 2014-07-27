c9d8ae83a6dc087d54d4d2d0867557bc071e6c74
LawlingBear <karl@transit.no>
2014-07-28 00:40:50 +0200

    Changed the argument variables and fixed some other things

--- src/arguments.h
+++ src/arguments.h
@@ -1,7 +1,5 @@
 #include <string>
-#include <map>
 #include <unordered_map>
-#include <set>
 #include <unordered_set>
 #include <iostream>
 #include <regex>
@@ -10,32 +8,32 @@ class Arguments
 {
 private:
 	// Map for pairs of string( --option argument )
-	map< string, string > pairs = new unordered_map< string, string >();
+	unordered_map< std::string, std::string > pairs;
 
 	// Map for flags( -abc )
-	set< char > flags  = new unordered_set< char >();
+	unordered_set< char > flags;
 
 public:
 
 	// Takes an array of Strings as an argument and constructs
 	// the maps of pairs and flags from it
-	Arguments( string[] args )
+	Arguments( int argc, char* argv[] )
 	{
-		parse_pairs( args );
-		parse_flags( args );
+		parse_pairs( int argc, char* argv[] );
+		parse_flags( int argc, char* argv[] );
 	}
 
 	// Returns the value of this key in the pairs map
-	string get_value( string key )
+	std::string get_value( std::string key )
 	{
 		return pairs.get( key );
 	}
 
 	// Returns the value of this key in the pairs map
 	// If that value is null, return the default value
-	string get_value( string key, string default_value )
+	std::string get_value( std::string key, std::string default_value )
 	{
-		string value = get_value( key );
+		std::string value = get_value( key );
 		return ( value == null ? default_value : value );
 	}
 
@@ -51,23 +49,23 @@ public:
 		return get_flag( new char( key ) );
 	}
 
-	void parse_pairs( string[] args )
+	void parse_pairs( int argc, char* argv[] )
 	{
-		for ( int i = 0 ; i < args.length ; i++ )
+		for ( int i = 0 ; i < argc ; i++ )
 		{
 			// Check if the string matches two hyphens
 			// followed by any number of characters and
 			// then check that there is at least one more
 			// string in args.
-			if ( args[i].matches( "--.*" ) && ( i + 1 <= args.length ) )
+			if ( argv[i].matches( "--.*" ) && ( i + 1 <= argc ) )
 			{
-				if ( args[i + 1].matches( "--.*" ) )
-					std::cout << "\"" << args[i] << " " << args[i + 1] + "\" is not an option pair.\n";
+				if ( argv[i + 1].matches( "--.*" ) )
+					std::cout << "\"" << argv[i] << " " << args[i + 1] << "\" is not an option pair.\n";
 
 				// Put the new argument pair in the map,
 				// overwriting any previous pair with the
 				// same key.
-				pairs.insert( args[i].substr( 2 ), args[i + 1] );
+				pairs.insert( argv[i].substr( 2 ), argv[i + 1] );
 
 				// Skip one string ahead over the pair
 				i++;
@@ -75,16 +73,16 @@ public:
 		}
 	}
 
-	void parse_flags ( string[] args )
+	void parse_flags ( int argc, char* argv[] )
 	{
-		for ( int i = 0 ; i < args.length ; i++ )
+		for ( int i = 0 ; i < argc ; i++ )
 		{
 			// Check that the string matches the flag option format
-			if ( args[i].matches( "-\\p{Alnum}?" ) )
+			if ( argv[i].matches( "-\\p{Alnum}?" ) )
 			{
 				// Add each flag in the string to the flags set
-				for (int j = 1 ; j < args[i].length() ; j++ )
-					flags.insert( args[i].at( j ) );
+				for (int j = 1 ; j < argv[i].length() ; j++ )
+					flags.insert( argv[i].at( j ) );
 			}
 		}
 	}
